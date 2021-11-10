2a37b49e529bef5b079ded04bac103ff11699620
Jake Causier <jake.causier@havebike.co.uk>
2021-11-10 23:51:11 +0000

    Fixed up config keys and added middleware for receiving localhost requests

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
-require('dotenv').config()
-
 const Discord = require('discord.js')
 const express = require('express')
 const parser = require('body-parser')
 const axios = require('axios')
 const _ = require('lodash')
 const config = require('./config.json')
-
 
 const app = express()
 app.use(parser.json())
@@ -20,27 +17,42 @@ const defaultProvider = {
 }
 const defaultColor = Discord.Util.resolveColor([241, 51, 51])
 
-// Listen for webhook to endpoints
-app.post("/announce", (req, res) => {
-  const channel = client.channels.cache.get(config.discord.announcement_channel_id)
+// Define API key middleware
+app.use((req, res, next) => {
+  if (config?.token !== null && req?.headers?.auth_token === config.token) {
+    next()
+  }
+  res.status(401).json('Missing auth_token header').end()
+})
 
-  if (channel) {
-    channel.send(`${req.body.type}: ${req.body.name} starting now! ${req.body.stream ? req.body.stream : null} @${req.body.target ? req.body.target : 'here'}`, {
+// Listen to endpoint to announce on Discord
+app.post("/announce", (req, res) => {
+  const channel = client.channels.cache.get(config.discord.announcement_channel_id) || null
+  const type = req.body.type || null
+  const title = req.body.title || null
+  const description = req.body.description || null
+  const shouldAnnounce = req.body.announce || null
+  const targetAnnounce = req.body.target || 'here'
+  const url = req.body.stream || null
+  const img = req.body.poster || null
+
+  if (channel && type && title) {
+    channel.send(`${type}: ${title} starting now! ${url || ''} ${shouldAnnounce ? `@${targetAnnounce}` : ''}`, {
       embed: new Discord.MessageEmbed({
-        title: req.body.name,
-        description: req.body.description,
-        image: req.body.poster ? { url: req.body.poster } : null,
+        url,
+        title,
+        description: description,
+        image: img ? { url: img } : null,
         provider: defaultProvider,
         color: defaultColor,
-        url: req.body.stream,
         footer: {
-          text: req.body.stream
+          text: title
         }
       })
     })
   }
 
-  res.status(200).end()
+  res.status(200).json({ status: 'OK' }).end()
 })
 
 // Trigger command on message
@@ -51,8 +63,10 @@ client.on('message', (message) => {
     return
   }
 
+  const prefix = config?.discord?.prefix || '='
+
   // Ensure the bot only replies when the input command is used
-  if (message.content.startsWith(config.prefix)) {
+  if (message.content.startsWith(prefix)) {
     const args = message.content.slice(1).split(' ')
     const action = args[0]
     const author = message.member
@@ -60,53 +74,52 @@ client.on('message', (message) => {
     // Help reply
     if (action === 'help') {
       const commands = [
-        `**${config.prefix}help** - Shows this help message`,
-        `**${config.prefix}votes** - Show the current film votes`,
-        `**${config.prefix}roll #number** - Roll a random number between 1 and #number`
+        `**${prefix}help** - Shows this help message`,
+        // `**${prefix}votes** - Show the current film votes`,
+        `**${prefix}roll #number** - Roll a random number between 1 and #number`
       ]
       return message.channel.send(`Hi ${author}, I'm Comfy-tan! Here are the commands you can use when mentioning me:${commands.map(val => `\n${val}`).join()}`)
     }
 
     // Film votes reply
-    if (action === 'votes') {
-      axios.get(`${process.env.COMFYTHEATRE_API_URL}/votes`)
-        .then(({ data }) => {
-          if (data.success) {
-            message.channel.send(`Here are the current film votes for you, ${author}...`, {
-              embed: new Discord.MessageEmbed({
-                title: 'Film Votes',
-                description: `Currently winning: **${data.data[0].title}** with ${data.data[0].votes} votes`,
-                thumbnail: {
-                  url: data.data[0].poster
-                },
-                provider: defaultProvider,
-                color: defaultColor,
-                fields: data.data.map((item) => {
-                  return {
-                    name: item.title,
-                    value: `${item.votes} votes`,
-                  }
-                })
-              })
-            })
-          } else {
-            message.reply('No votes have been submitted yet, or film voting is closed. Come back later!')
-          }
-        }).catch((err) => {
-          message.reply("I can't get that information at the moment. Go shout at Mic.")
-        })
-      return
-    }
+    // if (action === 'votes') {
+    //   axios.get(`${process.env.COMFYTHEATRE_API_URL}/votes`)
+    //     .then(({ data }) => {
+    //       if (data.success) {
+    //         message.channel.send(`Here are the current film votes for you, ${author}...`, {
+    //           embed: new Discord.MessageEmbed({
+    //             title: 'Film Votes',
+    //             description: `Currently winning: **${data.data[0].title}** with ${data.data[0].votes} votes`,
+    //             thumbnail: {
+    //               url: data.data[0].poster
+    //             },
+    //             provider: defaultProvider,
+    //             color: defaultColor,
+    //             fields: data.data.map((item) => {
+    //               return {
+    //                 name: item.title,
+    //                 value: `${item.votes} votes`,
+    //               }
+    //             })
+    //           })
+    //         })
+    //       } else {
+    //         message.reply('No votes have been submitted yet, or film voting is closed. Come back later!')
+    //       }
+    //     }).catch((err) => {
+    //       message.reply("I can't get that information at the moment. Go shout at Mic.")
+    //     })
+    //   return
+    // }
 
     // Dice roll
     if (action === 'roll') {
-      let value = args[1]
+      let value = args[1] || null
       const min = 2
       const max = 10000
-      const specialRolls = config.commands.roll.specialRolls
 
       if (!value || Number.isNaN(value)) {
-        return message.channel.send(`Please enter a valid number as the first parameter (ie. **${config.prefix}roll 6**)`)
+        return message.channel.send(`Please enter a valid number as the first parameter (ie. **${prefix}roll 6**)`)
       }
 
       value = Math.max(Number.parseInt(value))
@@ -115,22 +128,12 @@ client.on('message', (message) => {
       }
 
       const result = Math.floor(Math.random() * value)
-      if (specialRolls && specialRolls.length > 0) {
-        const valueMatch = _.find(specialRolls, { number: result })
-        if (typeof valueMatch !== 'undefined') {
-          // author.roles.add([valueMatch.grantRole])
-          //   .catch((err) => {
-          //     return message.channel.send(`Could not automatically add role. Go shout at Mic.`)
-          //   })
-          return message.channel.send(`${author} rolled **${result}**!!!`)
-        }
-      }
       return message.channel.send(`${author} rolled **${result}**`)
     }
   }
 })
 
-client.login(process.env.DISCORD_BOT_TOKEN)
-app.listen(process.env.EXPRESS_PORT, () => {
-  console.log(`Express server running on port ${process.env.EXPRESS_PORT}`)
+client.login(config.discord.token)
+app.listen(config.port || 3000, () => {
+  console.log(`Express server running on port ${config.port || 3000}`)
 })
